b06737843ae1679c17716c59d375cee62ebbc27e
Amanjainji <ajamanjain27@gmail.com>
2025-10-18 17:21:49 +0530

    push project

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,9 +1,6 @@
 import axios from "axios";
 
-const baseURL =
-  window.location.hostname === "localhost"
-    ? "http://localhost:8000"
-    : process.env.VITE_API_URL;
+const baseURL = import.meta.env.VITE_API_URL; 
 
 const api = axios.create({ baseURL });
 
